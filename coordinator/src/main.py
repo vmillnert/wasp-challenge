5816c1f7a8f03d02faa885cbda395798d7b1728c
David Gillsjö <david.gillsjo@math.lth.se>
2016-10-20 13:11:01 +0200

    * Implemented action client for move_base move actions

--- coordinator/src/main.py
+++ coordinator/src/main.py
@@ -6,10 +6,16 @@ import csv
 from std_msgs.msg import String
 from diagnostic_msgs.msg import KeyValue
 from rosplan_dispatch_msgs.msg import ActionDispatch,ActionFeedback
-from actionlib import SimpleActionClient
+from actionlib import SimpleActionClient, SimpleGoalState
 from coordinator.msg import MoveAction
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import sys
 
+class CoordinatorError(Exception):
+     def __init__(self, value):
+         self.value = value
+     def __str__(self):
+         return repr(self.value)
 
 class Coordinator:
     def __init__(self, wp_file):
@@ -24,10 +30,10 @@ class Coordinator:
 
         #Interface to Turtlebot
         ns_turtle = "turtlebot"
-        self.turtle_ac = SimpleActionClient(ns_turtle, MoveAction)
+        self.turtle_move_ac = SimpleActionClient(ns_turtle, MoveBaseAction)
 
         ns_drone = "beebop"
-        self.beebop_ac = SimpleActionClient(ns_drone, MoveAction)
+        self.beebop_move_ac = SimpleActionClient(ns_drone, MoveBaseAction)
 
     def read_waypoints(self, filename):
         waypoints = None
@@ -41,15 +47,51 @@ class Coordinator:
         # Check Action type and call correct functions.
         print "In dispatch feedback"
         if (msg.name == 'goto'):
-            self.action_goto(msg.parameters)
+                self.action_goto(msg.parameters)
 
     def action_goto(self, parameters):
         print "in goto"
+        obj = None
+        wp = None
+        for p in parameters:
+            if p.key == "obj":
+                obj = p.value
+            if p.key == "wp":
+                wp = p.value
+                
+        if obj == None:
+            raise CoordinatorError("No key obj in action dispatch parameter")
+        if wp == None:
+            raise CoordinatorError("No key wp in action dispatch parameter")
+
+
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+  
+        goal.target_pose.pose.position.x = self.waypoints[wp][0]
+        goal.target_pose.pose.position.y = self.waypoints[wp][1]
+
+        ac = None
+        if obj == "drone":
+            ac = self.beebop_move_ac
+        elif obj == "turtle":
+            ac = self.turtle_move_ac
+        if ac == None:
+            raise CoordinatorError("No action client exist for obj %s" % obj)
+
+        ac.send_goal(goal)
+        ac.wait_for_result()
+
+        if (ac.get_state() == SimpleGoalState.DONE):
+            print "Success!"
+        else:
+            print "Goal failed"
 
     def test_actions(self):
         dispatch_msg = ActionDispatch()
         dispatch_msg.name = "goto"
-        dispatch_msg.parameters = [KeyValue('obj','d0'), KeyValue('wp','wp1')]
+        dispatch_msg.parameters = [KeyValue('obj','drone'), KeyValue('wp','wp1')]
         tmp_pub = rospy.Publisher("/kcl_rosplan/action_dispatch", ActionDispatch, queue_size=10)
         rospy.sleep(0.1)
         tmp_pub.publish(dispatch_msg)
