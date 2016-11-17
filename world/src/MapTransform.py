e7717450f92197034ef6e3657d2e8c6d9d244826
Per Skarin <per.skarin@gmail.com>
2016-11-17 14:59:55 +0000

    world: added periodic update of transforms

--- world/src/MapTransform.py
+++ world/src/MapTransform.py
@@ -29,15 +29,15 @@ import rospy
 import tf
 import math
 import numpy
+from threading import Thread,Event,Lock
 from world.msg import TagRef
 
-broadcaster = tf.TransformBroadcaster()
-
+# This callback is invoked when a tag (landmark) is spotted
 def locatedCallback(tagref):
-  global listener
+  global listener   # Created in listener()
+  global transforms  # A dictionary of transforms to publish
 
   try:
-    # TODO: Could it be necessary to wait for transform?
     listener.waitForTransform(tagref.global_frame, tagref.tag_frame, tagref.pose.header.stamp, rospy.Duration(0.5))
     (trans_gt, rot_gt) = listener.lookupTransform(tagref.global_frame, tagref.tag_frame, tagref.pose.header.stamp)
     mat_map = tf.transformations.concatenate_matrices(
@@ -63,30 +63,47 @@ def locatedCallback(tagref):
     mat_map_to_odom = numpy.dot(mat_map, mat_odom_inv)
     trans = tf.transformations.translation_from_matrix(mat_map_to_odom)
     rot = tf.transformations.quaternion_from_matrix(mat_map_to_odom)
-    
-    sendTransform(tagref.global_frame, tagref.target_frame, trans, rot)
+   
+    lock.acquire() 
+    key=tagref.global_frame+","+tagref.tag_frame+","+tagref.target_frame
+    transforms[key] = (tagref.global_frame, tagref.target_frame, trans, rot)
+    lock.release()
+
   except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
     print("Transformation error")
     print(e)
 
+# Publishes the transform
 def sendTransform(fromFrame, toFrame, trans,rot):
   global broadcaster
-  broadcaster.sendTransform(trans,
-                   rot,
-                   rospy.Time.now(),
-                   toFrame,
-                   fromFrame)
+  broadcaster.sendTransform(trans, rot, rospy.Time.now(), toFrame, fromFrame)
 
-def listener():
-  global listener
+# Periodically broadcasts the transform
+def broadcastThread():
+  global transforms, lock
+  rate = rospy.Rate(10) # 10hz
+  while not rospy.is_shutdown():
+    lock.acquire()
+    for k,v in transforms.iteritems():
+      sendTransform(v[0], v[1], v[2], v[3])
+    lock.release()
+    rate.sleep()
+
+# Create the tag reference listener
+def startMapTransform():
+  global listener       # Used by locatedCallback
+  global broadcaster    # Used by sendTransform
+  global transforms     # Published periodically
+  global lock           # Synchronize locatedCallback and broadcast thread
+
+  lock = Lock()
+  transforms = {}
+  broadcaster = tf.TransformBroadcaster()
   rospy.init_node('tagref', anonymous=False)
-
-  listener = tf.TransformListener() # init_node needs to happen first
-
-  rospy.Subscriber('tagref', TagRef, locatedCallback) # TODO: Name and type of topic
-
+  listener = tf.TransformListener()           # Note: init_node must happen first
+  Thread(target=broadcastThread).start()
+  rospy.Subscriber('tagref', TagRef, locatedCallback)
   rospy.spin()
 
 if __name__ == "__main__":
-  listener()
-
+  startMapTransform()
