76514add95becc33087d4e2ae7ade26a211cda73
master.wasp <master@wasp.wasp>
2016-11-22 08:40:24 +0100

    The canges made from last time

--- coordinator/src/main.py
+++ coordinator/src/main.py
@@ -211,6 +211,8 @@ class Coordinator:
             rospy.loginfo("server timeout")
 
         goal.target_pose.header.frame_id = self.coordinate_frame
+        if obj == "drone":
+            goal.target_pose.header.frame_id = "bebop/odom"
         goal.target_pose.header.stamp = rospy.Time.now()
 
         goal.target_pose.pose.position.x = self.waypoints[wp][0]
