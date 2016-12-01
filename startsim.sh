33923c201b19a4f9c82085a16564e7baf4a53059
Per Skarin <per.skarin@gmail.com>
2016-12-01 14:59:21 +0000

    sim: Moved turtle start position. Updated README.

--- startsim.sh
+++ startsim.sh
@@ -3,10 +3,11 @@
 mkdir -p logs;
 echo Launch the world
 roslaunch world simulator.launch >logs/simulator.log 2>&1 &
-sleep 10;
+sleep 5;
 echo Bring in the turtle
+export ROBOT_INITIAL_POSE="-x -0.5 -y -0.5";
 roslaunch wasp_g1_start start_turtlebot_sim.launch >logs/turtle.log 2>&1 &
-sleep 10;
+sleep 5;
 echo Add drone
 roslaunch world ardrone_sim.launch >logs/drone.log 2>&1;
 
