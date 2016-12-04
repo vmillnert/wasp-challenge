80c53dad64fd8edee964aaeaf5c92999d3e55b80
Per Skarin <per.skarin@gmail.com>
2016-12-04 17:39:49 +0000

    startsim: optional rviz, delay

--- startsim.sh
+++ startsim.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 declare -a PIDS=()
 
+
+if [ ! $DELAY ];
+then
+  DELAY=5;
+fi;
+
 mkdir -p logs;
 echo Launch the world
 roslaunch world simulator.launch >logs/simulator.log 2>&1 &
 PIDS+=($!);
-sleep 5;
+sleep ${DELAY};
 
 rosparam set /available_drones ["drone"]
 rosparam set /available_turtlebots ["turtle"]
@@ -16,7 +22,7 @@ then
   export ROBOT_INITIAL_POSE="-x -0.5 -y -0.5";
   roslaunch wasp_g1_start start_turtlebot_sim.launch >logs/turtle.log 2>&1 &
   PIDS+=($!);
-  sleep 5;
+  sleep ${DELAY};
 fi;
 
 if [ ! $NODRONE ];
@@ -24,7 +30,7 @@ then
   echo Add drone
   roslaunch world ardrone_sim.launch >logs/drone.log 2>&1 &
   PIDS+=($!);
-  sleep 5;
+  sleep ${DELAY};
   DRONESIM= rosrun bebop_controller BebopActionServer.py > logs/dronecontroller.log 2>&1 &
   PIDS+=($!);
 fi;
@@ -33,8 +39,13 @@ echo "Add map"
 roslaunch world sim_map.launch >logs/map.log 2>&1 &
 PIDS+=($!);
 
-echo "RViz"
-roslaunch turtlebot_rviz_launchers view_navigation.launch
+if [ ! $NORVIZ ];
+then
+  echo "RViz"
+  roslaunch turtlebot_rviz_launchers view_navigation.launch >logs/rviz.log 2>&1 &
+fi;
+
+read -p "Press enter to exit";
 
 echo "Killing of nodes";
 kill ${PIDS[@]};
